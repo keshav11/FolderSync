4c5c3c63e0caac7e0e3bc28bf071b58fdd132e59
Keshav Gupta <keshav1032@gmail.com>
2018-03-15 14:27:59 -0400

    added multiple folder registration

--- src/foldersync/Main.java
+++ src/foldersync/Main.java
@@ -1,29 +1,54 @@
 package foldersync;
 
 import java.io.IOException;
-import java.io.InterruptedIOException;
 import java.nio.file.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
+
+    private static Map<WatchKey, Path> watchKeys = new HashMap<>();
+
+    private static boolean syncDirs(WatchEvent event, WatchKey eventKey) {
+        Path modFile = (Path) event.context();
+        Path modDir = watchKeys.get(eventKey);
+
+        System.out.println(event.kind() + " in directory " + modDir);
+        System.out.println(event.kind() + " for file " + modFile);
+
+        return true;
+    }
+
+    private static void registerDir(WatchService folderWatcher, String dir) throws IOException{
+        Path path = Paths.get(dir).toRealPath();
+
+        WatchKey folderKey = path.register(folderWatcher,
+                StandardWatchEventKinds.ENTRY_CREATE,
+                StandardWatchEventKinds.ENTRY_DELETE,
+                StandardWatchEventKinds.ENTRY_MODIFY,
+                StandardWatchEventKinds.OVERFLOW);
+
+        watchKeys.put(folderKey, path);
+
+    }
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
         WatchService folderWatcher = FileSystems.getDefault().newWatchService();
 
-        Path path = Paths.get("./test/");
+        String dir1 = "./test1/";
+        String dir2 = "./test2/";
 
-        path.register(folderWatcher, StandardWatchEventKinds.ENTRY_CREATE);
-        path.register(folderWatcher, StandardWatchEventKinds.ENTRY_DELETE);
-        path.register(folderWatcher, StandardWatchEventKinds.ENTRY_MODIFY);
+        registerDir(folderWatcher, dir1);
+        registerDir(folderWatcher, dir2);
 
         boolean complete = false;
-        while (!complete) {
 
+        while (!complete) {
             WatchKey key = folderWatcher.take();
             for (WatchEvent event : key.pollEvents()) {
-                System.out.println(event.kind());
+                syncDirs(event, key);
             }
-            complete = !key.reset();
         }
     }
 }
