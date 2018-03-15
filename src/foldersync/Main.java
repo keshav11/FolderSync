faa7c880e1fa039a26dee9108e124c9d8d4f9947
Keshav Gupta <keshav1032@gmail.com>
2018-03-15 01:18:42 -0400

    added basic folder watch

--- src/foldersync/Main.java
+++ src/foldersync/Main.java
@@ -1,8 +1,29 @@
 package foldersync;
 
+import java.io.IOException;
+import java.io.InterruptedIOException;
+import java.nio.file.*;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
+        WatchService folderWatcher = FileSystems.getDefault().newWatchService();
+
+        Path path = Paths.get("./test/");
+
+        path.register(folderWatcher, StandardWatchEventKinds.ENTRY_CREATE);
+        path.register(folderWatcher, StandardWatchEventKinds.ENTRY_DELETE);
+        path.register(folderWatcher, StandardWatchEventKinds.ENTRY_MODIFY);
+
+        boolean complete = false;
+        while (!complete) {
+
+            WatchKey key = folderWatcher.take();
+            for (WatchEvent event : key.pollEvents()) {
+                System.out.println(event.kind());
+            }
+            complete = !key.reset();
+        }
     }
 }
