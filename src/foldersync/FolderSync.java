e579972958af7cd2276c33fb587c0c449ce51920
Keshav Gupta <keshav1032@gmail.com>
2018-03-15 21:59:29 -0400

    refatored code

--- src/foldersync/FolderSync.java
+++ src/foldersync/FolderSync.java
@@ -1,15 +1,11 @@
 package foldersync;
 
 import java.io.IOException;
-import java.nio.file.*;
-
 public class FolderSync {
-
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
         Monitor monitor = new Monitor();
-
         for(String dir: args)
             monitor.registerDir(dir);
 
--- src/foldersync/Monitor.java
+++ src/foldersync/Monitor.java
@@ -7,26 +7,27 @@ import java.util.Map;
 
 public class Monitor {
 
-    Map<WatchKey, Path> watchKeys = new HashMap<>();
-    WatchService folderWatcher;
+    private Map<WatchKey, Path> watchKeys;
+    private WatchService folderWatcher;
 
-    public Monitor() throws IOException {
+    Monitor() throws IOException {
+        watchKeys = new HashMap<>();
         folderWatcher = FileSystems.getDefault().newWatchService();
     }
 
-    boolean copy(Path modFile,  Path modPath) throws IOException {
+    void copy(Path modFile,  Path modPath) throws IOException {
         for(Path dest: watchKeys.values()) {
             Path destPath = Paths.get(dest.toString(), modFile.toString());
             System.out.println("Source: " + modPath.toString());
             System.out.println("Destination: " + destPath.toString());
             if(modPath.compareTo(destPath) != 0)
-                Files.copy(modPath, destPath, StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.COPY_ATTRIBUTES);
+                Files.copy(modPath, destPath,
+                        StandardCopyOption.REPLACE_EXISTING,
+                        StandardCopyOption.COPY_ATTRIBUTES);
         }
-
-        return true;
     }
 
-    boolean syncDirs(WatchEvent event, WatchKey eventKey) throws IOException {
+    void syncDirs(WatchEvent event, WatchKey eventKey) throws IOException {
         Path modFile = (Path) event.context();
         Path modDir = watchKeys.get(eventKey);
         Path modPath = Paths.get(modDir.toString(), modFile.toString());
@@ -53,13 +54,10 @@ public class Monitor {
                 }
             }
         }
-
-        return true;
     }
 
     void registerDir(String dir) throws IOException{
         Path path = Paths.get(dir).toRealPath();
-
         WatchKey folderKey = path.register(folderWatcher,
                 StandardWatchEventKinds.ENTRY_CREATE,
                 StandardWatchEventKinds.ENTRY_DELETE,
@@ -72,11 +70,13 @@ public class Monitor {
 
     void startMonitoring() throws InterruptedException, IOException {
         boolean complete = false;
+
         while (!complete) {
             WatchKey key = folderWatcher.take();
             for (WatchEvent event : key.pollEvents()) {
                 this.syncDirs(event, key);
             }
+
             complete = !key.reset();
         }
     }
