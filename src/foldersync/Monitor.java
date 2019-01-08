63713524c5966dbd1f215b8dce84a05d60954c57
Keshav Gupta <keshav1032@gmail.com>
2019-01-08 22:20:39 +0530

    Added some folder validation

--- src/foldersync/Monitor.java
+++ src/foldersync/Monitor.java
@@ -1,11 +1,11 @@
 package foldersync;
 
-import java.util.List;
-import java.util.ArrayList;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Monitor {
@@ -22,13 +22,12 @@ public class Monitor {
 
     private void copy(Path modFile, Path modDir) throws IOException {
         Path modPathSrc = Paths.get(modDir.toString(), modFile.toString());
-        for(String destDir: backupFoldersMap.get(modDir.toString())) {
+        for (String destDir : backupFoldersMap.get(modDir.toString())) {
             Path modPathDest = Paths.get(destDir, modFile.toString());
             System.out.println("[Copy]: Source " + modPathSrc.toString());
             System.out.println("[Copy]: Destination " + modPathDest.toString());
-            Files.copy(modPathSrc, modPathDest,
-            StandardCopyOption.REPLACE_EXISTING,
-            StandardCopyOption.COPY_ATTRIBUTES);
+            Files.copy(modPathSrc, modPathDest, StandardCopyOption.REPLACE_EXISTING,
+                    StandardCopyOption.COPY_ATTRIBUTES);
         }
     }
 
@@ -38,14 +37,12 @@ public class Monitor {
 
         System.out.println("[Event]: " + event.kind() + " in directory " + modDir + " for file " + modFile);
 
-        if(event.kind() == StandardWatchEventKinds.ENTRY_CREATE) {
+        if (event.kind() == StandardWatchEventKinds.ENTRY_CREATE) {
             copy(modFile, modDir);
-        }
-        else if(event.kind() == StandardWatchEventKinds.ENTRY_MODIFY) {
+        } else if (event.kind() == StandardWatchEventKinds.ENTRY_MODIFY) {
             copy(modFile, modDir);
-        }
-        else if(event.kind() == StandardWatchEventKinds.ENTRY_DELETE) {
-            for(String backupDir: backupFoldersMap.get(modDir.toString())) {
+        } else if (event.kind() == StandardWatchEventKinds.ENTRY_DELETE) {
+            for (String backupDir : backupFoldersMap.get(modDir.toString())) {
                 Path toDelPath = Paths.get(backupDir, modFile.toString());
                 System.out.println("[Delete]: " + toDelPath.toString());
                 try {
@@ -57,21 +54,44 @@ public class Monitor {
         }
     }
 
-    public void registerDir(String[] dirs) throws IOException{
+    private boolean validateDirs(String[] dirs) {
+        if (dirs.length < 2) {
+            System.out.println("At least two folder paths are required.");
+            return false;
+        }
+
+        for (String dir : dirs) {
+            File dirFil = new File(dir);
+            if (!dirFil.exists()) {
+                System.out.println(String.format("Path %s does not exists", dir));
+                return false;
+            }
+
+            if (!dirFil.isDirectory()) {
+                System.out.println(String.format("Path %s is not a directory", dir));
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void registerDir(String[] dirs) throws IOException {
+        if (!validateDirs(dirs))
+            return;
+
         String master = dirs[0];
         Path path = Paths.get(master).toRealPath();
-            WatchKey folderKey = path.register(folderWatcher,
-                                StandardWatchEventKinds.ENTRY_CREATE,
-                                StandardWatchEventKinds.ENTRY_DELETE,
-                                StandardWatchEventKinds.ENTRY_MODIFY,
-                                StandardWatchEventKinds.OVERFLOW);
-        
+        WatchKey folderKey = path.register(folderWatcher, StandardWatchEventKinds.ENTRY_CREATE,
+                StandardWatchEventKinds.ENTRY_DELETE, StandardWatchEventKinds.ENTRY_MODIFY,
+                StandardWatchEventKinds.OVERFLOW);
+
         watchKeys.put(folderKey, path);
         List<String> backupList = new ArrayList<>();
         for (int i = 1; i < dirs.length; i++) {
             backupList.add(dirs[i]);
         }
-        
+
         backupFoldersMap.put(path.toString(), backupList);
     }
 
